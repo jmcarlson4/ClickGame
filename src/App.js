eb25746b21ff2f280195ed0dbb81374c9293db4d
unknown <jodimcarlson4@gmail.com>
2019-02-09 13:04:45 -0600

    created files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Jumbotron from './components/Jumbotron';
+
+
+function App()  {
+  
+    return (
+    <Jumbotron/>);
+    
+    
+};
+
+export default App;
---
+++ src/components/Jumbotron.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Jumbotron() {
+    return (<div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Clicky Game!</h1>
+      <p class="lead">Click on an image to earn points, but do not click on an image more than once!</p>
+    </div>
+    </div>);
+  }
+  
+  export default Jumbotron;
