8257d07d549d106c578ee56fc1278b45e090a48b
unknown <jodimcarlson4@gmail.com>
2019-02-10 17:51:52 -0600

    deleted unneeded files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Jumbotron from './components/Jumbotron';
+import Score from './components/Score';
+import SearchResults from './components/SearchResults';
 
 
 function App()  {
