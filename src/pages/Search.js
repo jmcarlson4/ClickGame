75084ae13d1e3302db99648f0f514b01e666572c
unknown <jodimcarlson4@gmail.com>
2019-02-10 08:44:46 -0600

    moved and added files

---
+++ src/pages/Search.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import API from "../util/api";
+import SearchResults from "../components/SearchResults";
+
+class Search extends Component {
+    state = {
+        results: []
+    }
+
+    componentDidMount() {
+        API.getRandomDog()
+            .then(res => this.setState({ results }))
+            .catch(err => console.log(err));
+    };
+
+    render() {
+        return (
+            <SearchResults results={this.state.results} />
+        );
+    }
+}
+export default Search;
+
+
+
